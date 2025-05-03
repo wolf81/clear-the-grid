0314b313a9b6b52fd5f7194c0b212eb1becf3149
Wolfgang Schreurs <wolfgang.schreurs@gmail.com>
2025-05-03 17:42:00 +0700

    Use transformation matrix in Projector to scale & translate

--- src/projector.lua
+++ src/projector.lua
@@ -5,15 +5,17 @@ local Projector = {}
 Projector.new = function(virtual_w, virtual_h)
     local canvas = love.graphics.newCanvas(virtual_w, virtual_h)
 
-    local scale = 1
-    local ox, oy = 0, 0 -- offsets for letterboxing
+    local transform = love.math.newTransform()
 
     local resize = function(self, window_w, window_h)
-        local sx, sy = window_w / virtual_w, window_h / virtual_h
-        scale = math.min(sx, sy)
+        transform:reset()
 
-        ox = math.floor((window_w - virtual_w * scale) / 2)
-        oy = math.floor((window_h - virtual_h * scale) / 2)
+        local sx, sy = window_w / virtual_w, window_h / virtual_h
+        transform:scale(sx, sy)
+
+        local ox = floor((window_w - virtual_w * sx) / 2)
+        local oy = floor((window_h - virtual_h * sy) / 2)
+        transform:translate(ox, oy)
     end
 
     local attach = function(self)
@@ -22,6 +24,7 @@ Projector.new = function(virtual_w, virtual_h)
         -- draw on canvas
         love.graphics.setCanvas(canvas)
         love.graphics.clear(0, 0, 0) -- start with black canvas
+
         love.graphics.origin()
     end
 
@@ -33,14 +36,13 @@ Projector.new = function(virtual_w, virtual_h)
 
         -- render canvas to screen
         love.graphics.push()
-        love.graphics.translate(ox, oy)
-        love.graphics.scale(scale)
+        love.graphics.applyTransform(transform)
         love.graphics.draw(canvas)
         love.graphics.pop()
     end
 
     local toWorld = function(self, x, y)
-        return floor(x * 1/scale), floor(y * 1/scale)
+        return transform:inverseTransformPoint(x, y)
     end
     
     -- setup initial size
